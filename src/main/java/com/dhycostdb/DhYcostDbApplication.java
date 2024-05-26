bfeaddcab19ca612f788c7dfcb7742d1d3915622
Hiroaki.Yamamoto <noir.noir@hotmail.co.jp>
2024-05-27 07:51:11 +0900

    Springプロジェクト作成直後

---
+++ src/main/java/com/dhycostdb/DhYcostDbApplication.java
@@ -0,0 +1,13 @@
+package com.dhycostdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhYcostDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhYcostDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhycostdb/DhYcostDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhycostdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhYcostDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
